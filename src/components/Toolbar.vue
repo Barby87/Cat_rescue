d68dff3d5a4847e85f6d88a271c4e9de2c59cd7b
Barby87 <bcaceres.sepulveda@hotmail.com>
2020-06-24 12:12:07 -0400

    Toolbar, footer and routes

---
+++ src/components/Toolbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-card color="grey lighten-4" flat tile>
+        <v-toolbar color="teal" dark>
+          <v-container>
+          <v-toolbar-title>Cat Rescue</v-toolbar-title>
+          <v-spacer></v-spacer> 
+          <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-btn icon>
+            <v-icon>mdi-magnify</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+          </v-btn>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn> -->
+          <v-toolbar-items class="hidden-sm-and-down">
+            <v-tab to="/">HOME</v-tab>
+            <v-tab to="/login">LOGIN</v-tab>
+            <v-tab to="/signup">REGISTRO</v-tab>
+          </v-toolbar-items>
+          </v-container>
+        </v-toolbar>
+    </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'Toolbar',
+
+    data: () => ({
+     
+    })
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,22 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-layout wrap>
+      <!-- row -->
+      <v-flex class="primary" xs6 md2 >
+        <h1>12</h1>
+      </v-flex>
+      <v-flex class="secondary" xs6>
+        <h1>6</h1>
+      </v-flex>
+       <v-flex class="error" xs6>
+        <h1>6</h1>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
